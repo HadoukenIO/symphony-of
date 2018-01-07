9e42f64b05c675d98f6bd9c024b477d4cc2c64d4
Xavier ONeil <xavier@openfin.co>
2018-01-06 19:16:26 -0500

    s3 stuff

--- deploy.js
+++ deploy.js
@@ -1,25 +1,15 @@
 const repl = require('repl');
 const { exec, execSync } = require('child_process');
 
+console.log(`running in ${process.cwd()}`);
 
-const msg = 'message';
+const replServer = repl.start({ eval: reader })
+const depgen = deploy();
 
-let depgen;
-let replServer;
-
-// const { exec } = require('child_process');
-// exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.log(`stderr: ${stderr}`);
-// });
-
+replServer.question('start deploy? (will reset --hard HEAD in current environment) ', reader);
 
 function reader(input) {
-    if (input !== 'y') { process.exit() }
+    if (input !== 'y') { process.exit(1) }
     depgen.next();
 }
 
@@ -92,30 +82,30 @@ function* deploy() {
     replServer.question('ok? ', reader);
     yield;
 
+    command = `aws s3 cp ./public s3://cdn.openfin.co/demos/symphony-of-staging/ --recursive`;
+    exec(command, (error, stdout) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            process.exit(1)
+            return;
+        }
+        console.log(stdout);
+        replServer.question('ok? ', reader);
+    });
+    yield;
+
+    command = `aws cloudfront create-invalidation --distribution-id E16N7NZUXTHZCF --paths /demos/symphony-of-staging/*`;
+    exec(command, (error, stdout) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            process.exit(1)
+            return;
+        }
+        console.log(stdout);
+        replServer.question('ok? ', reader);
+    });
+    yield;
+
+    console.log('successful deployment, go test it!');
+    process.exit(0);
 }
-
-// console.log(`start deploy?`);
-replServer = repl.start({ prompt: '> ', eval: reader })
-depgen = deploy();
-// replServer.write('start deploy? ');
-replServer.question('start deploy? (will call git reset --hard HEAD in current environment) ', reader)
-
-
-
-/*
-
-///// * `git fetch --all`
-/////* `git checkout upstream/develop`
-* check the version in `package.json`
-* update the `containerVer` in `main.js` to one patch greater and save (ex. 0.0.14 -> 0.0.15)
-* `npm run build staging`
-* `git add .` (just some urls could _possibly_ change here + the version)
-* `git diff` (sanity check)
-* `npm version patch --force -m "staging build %s: <brief description of changes>"`
-* push tags upstream `git push upstream --tags`
-* push branch upstream `git push upstream HEAD:develop`
-* copy the contents of the `public/` directory to the staging s3 bucket  (`https://s3.amazonaws.com/cdn.openfin.co/demos/symphony-of-staging`)
-`aws s3 cp [local path]/public s3://cdn.openfin.co/demos/symphony-of-staging/ --recursive --exclude "node_modules/*" --exclude ".git/*"`
-* Invalidate the cloudfront cdn for `/demos/symphony-of-staging/*`
-
-*/
