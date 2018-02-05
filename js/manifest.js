7fae721a5a60eed16972f0a2ae647de6511b910f
Michael Coates <MichaelMCoates@gmail.com>
2018-02-05 16:15:11 -0500

    [Notifications] move symphonyNotifications flag to customData

--- js/manifest.js
+++ js/manifest.js
@@ -6,10 +6,13 @@ fin.desktop.Application.getCurrent().getManifest(function (manifest) {
   window.localStorage.setItem('notificationsVersion', "V1");
   window.localStorage.setItem('notificationsHeight', 80);
   
-    if (manifest.startup_app.symphonyNotifications == "V2") {
+  if (manifest.startup_app.customData) {
+    if (manifest.startup_app.customData.symphonyNotifications === "V2") {
       window.localStorage.setItem('notificationsVersion', "V2");
       window.localStorage.setItem('notificationsHeight', 60);
     }
+  }
+  
 });
 
 if (window.localStorage.getItem('notificationsLocation') === null) {
