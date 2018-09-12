0a6aadf25364d199969a675ffd20abab05ed3383
Thomas O'Connor <tom@openfin.co>
2018-09-12 09:28:07 -0400

    Refactored popouts logic (#123)
    * refactored popouts logic
    
    * undo note changes
    
    * undo note changes
    
    * undo note changes
    
    * undo note changes
    
    * put main window logic back in
    
    * wrong variable
    
    * main window always on top

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ window.SYM_API = {
     ScreenSnippet,
 
     setBadgeCount:function(number) {
-        let win = fin.desktop.Window.getCurrent();
+        let win = fin.desktop.Application.getCurrent().getWindow();
         if (number > 0) {
             let n = number > 9 ? '9+' : number;
             win.updateOptions({ icon: `${window.targetUrl}icon/icon${n}.png` },() => {win.flash();},() => {console.log("update options failed");});
@@ -17,44 +17,22 @@ window.SYM_API = {
         };
     },
     activate:function(windowName) {
-        window.popouts = JSON.parse(window.localStorage.getItem('wins')) || {};        
-        let mainApp = fin.desktop.Application.getCurrent();
-        if(windowName === 'main') {
-            let mainWin = mainApp.getWindow();
-            window.winFocus(mainWin);
-        }
-        for (let pop of Object.keys(window.popouts)) {
-            if(window.popouts[pop].symName === windowName) {
-                let popWin = fin.desktop.Window.wrap(mainApp.uuid, window.popouts[pop].name);
-                window.winFocus(popWin);
+        var app = fin.desktop.Application.getCurrent();
+        var ofWin = fin.desktop.Window.wrap(app.uuid, windowName);
+
+        ofWin.getState(state => {
+            if (state === 'minimized') {
+                ofWin.restore(() => {ofWin.setAsForeground();},e=>console.log(e));                            
+            } else {
+                ofWin.setAsForeground();    
             }
-        }
+        })
     },
     //undoced
     registerLogger:function() {
     },
     registerBoundsChange:function(cb) {
-        // fin.desktop.Application.getCurrent().addEventListener("window-created", obj => {
-        //     if(obj.name !== obj.uuid && !obj.name.includes('Notifications') && obj.name !== 'queueCounter') {    
-        //         fin.desktop.Window.wrap(obj.uuid, obj.name).addEventListener("bounds-changed", win => {
-        //             for (let pop of Object.keys(window.popouts)) {
-        //                 if(window.popouts[pop].name === obj.name) {
-        //                     window.popouts[pop] = Object.assign(window.popouts[pop], win)
-        //                     if(cb) {
-        //                         // Does this callback do anything? In the symphony API spec... (need to be )
-        //                         cb({x:win.left,
-        //                             y:win.top,
-        //                             width:win.width,
-        //                             height:win.height,
-        //                             windowName:window.popouts[pop].symName
-        //                         });
-        //                     }
-        //                 }
-        //             }
-        //             window.localStorage.setItem('wins', JSON.stringify(window.popouts));       
-        //         })
-        //     }
-        // });
+        window.saveBounds = cb;
     },
     getVersionInfo: function() {
         return new Promise((resolve, reject) => {
